ab9d9dedb105c4de08648d70279bde85624f2c2a
yekdjxjxjxjd <121597693+yekdjxjxjxjd@users.noreply.github.com>
2023-06-04 14:49:07 +0800

    Update mainwindow.h

--- trail/mainwindow.h
+++ trail/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include "basic.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,10 +16,39 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+
+
+
+    QGraphicsScene* scene;
+
+    Player* player;
+    Enemy* enemy;
+    QTimer* timer;
+    QGraphicsRectItem* item_player;
+    QGraphicsRectItem* item_enemy;
+    QGraphicsRectItem* item_bullet;
+
+    void drawBulletTrajectory(Bullet* bullet);
+    void mousePressEvent(QMouseEvent *event);
+    double SPEED_PLAYER = 5;
+    double SPEED_ENEMY = 2;
+    // mainwindow.h
+ public slots:
+    void shoot(double mousex,double mousey);
+
+private slots:
+    void movePlayer();
+    void moveEnemy();
+
 };
+
+
+
+
 #endif // MAINWINDOW_H
