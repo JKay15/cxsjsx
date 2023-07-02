7337655dab8d865be69ff8ee18bb48c6f03e1470
acetocarmineboss <133663873+acetocarmineboss@users.noreply.github.com>
2023-07-02 11:14:35 +0800

    Create shop.h

---
+++ shop.h
@@ -0,0 +1,37 @@
+// shop.h
+#ifndef SHOP_H
+#define SHOP_H
+
+#include <vector>
+#include <string>
+#include "actions.h"
+#include "game.h"
+#include "helpers.h"
+#include "levels.h"
+#include "sounds.h"
+
+// 商店物品结构体
+struct ShopItem {
+    int cost; // 花费
+    std::string name; // 名称
+    std::string description; // 描述
+    void (*purchase)(); // 购买函数
+};
+
+// 商店结构体
+struct Shop {
+    std::vector<Ritual> rituals; // 仪式列表
+    std::vector<ShopItem> items; // 物品列表
+    int selectedIndex; // 当前选中的物品索引
+};
+
+extern Shop shop;
+
+void buy(); // 购买函数
+void selectShopIndex(int step); // 选择商店物品索引函数
+void enterShop(); // 进入商店函数
+void exitShop(); // 退出商店函数
+void restockShop(); // 重新进货函数
+std::vector<ShopItem> createRitualItems(); // 创建仪式物品列表函数
+
+#endif
