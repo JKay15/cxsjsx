cb6ef84f8d1861f7f9bf2e4ff014548863b1e2c7
acetocarmineboss <133663873+acetocarmineboss@users.noreply.github.com>
2023-07-02 11:49:02 +0800

    Create objects.h

---
+++ objects.h
@@ -0,0 +1,33 @@
+// objects.h
+#ifndef OBJECTS_H
+#define OBJECTS_H
+
+#include "game.h"
+#include "tags.h"
+#include "helpers.h"
+#include "behaviours.h"
+#include "actions.h"
+
+GameObject Corpse();
+GameObject Player();
+GameObject Spell();
+GameObject BleedSpell();
+GameObject LightningSpell();
+GameObject Skeleton();
+GameObject SkeletonLord();
+GameObject Villager();
+GameObject Bandit();
+GameObject TheKing();
+GameObject Champion();
+GameObject ShellKnight();
+GameObject Monk();
+GameObject Archer();
+GameObject Piper();
+GameObject Rat();
+GameObject RageKnight();
+GameObject RoyalGuard();
+GameObject RoyalGuardOrb();
+GameObject Wizard();
+GameObject Portal();
+
+#endif
