2b73f3facbab1909c9e81a5fb63e1ab2c4f4bcdb
acetocarmineboss <133663873+acetocarmineboss@users.noreply.github.com>
2023-07-02 11:19:34 +0800

    Create actions.h

---
+++ actions.h
@@ -0,0 +1,16 @@
+// actions.h
+#ifndef ACTIONS_H
+#define ACTIONS_H
+
+#include <string>
+#include "game.h"
+#include "helpers.h"
+#include "objects.h"
+#include "tags.h"
+
+void Damage(GameObject& object, int amount, GameObject* dealer = nullptr); // 造成伤害函数
+void Die(GameObject& object, GameObject* killer = nullptr); // 死亡函数
+void Cast(); // 施法函数
+void Resurrect(); // 复活函数
+
+#endif
