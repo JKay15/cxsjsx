28fb7377b66c2021a9491610230e6dd8ff38b8e8
JKay Xiong <xiongjk15@gmail.com>
2023-07-03 10:45:58 +0800

    Add files via upload

---
+++ tags.h
@@ -0,0 +1,11 @@
+#ifndef TAGS_H
+#define TAGS_H
+
+const int MOBILE = 1 << 0;
+const int LIVING = 1 << 1;
+const int UNDEAD = 1 << 2;
+const int CORPSE = 1 << 3;
+const int SPELL = 1 << 4;
+const int PLAYER = 1 << 5;
+
+#endif // TAGS_H
