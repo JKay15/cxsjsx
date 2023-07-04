585abea9ad2a03487cd7a9b9fb26b7668518a6cb
JKay Xiong <xiongjk15@gmail.com>
2023-07-05 00:52:00 +0800

    Update actions.h

--- actions.h
+++ actions.h
@@ -1,4 +1,3 @@
-// actions.h
 #ifndef ACTIONS_H
 #define ACTIONS_H
 
@@ -7,10 +6,17 @@
 #include "helpers.h"
 #include "objects.h"
 #include "tags.h"
+#include <chrono>
+#include <thread>
+#include "sprites.h"
+#include "fx.h"
 
-void Damage(GameObject& object, int amount, GameObject* dealer = nullptr); // 造成伤害函数
-void Die(GameObject& object, GameObject* killer = nullptr); // 死亡函数
+namespace actions{
+void Damage(GameObject* object, int amount, GameObject* dealer = nullptr); // 造成伤害函数
+void Die(GameObject* object, GameObject* killer = nullptr); // 死亡函数
 void Cast(); // 施法函数
 void Resurrect(); // 复活函数
+}
 
-#endif
+#endif // ACTIONS_H
+
