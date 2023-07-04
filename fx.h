5394dc1e5ed007edb79df89aad38b192f2f1d38d
JKay Xiong <xiongjk15@gmail.com>
2023-07-05 00:55:16 +0800

    Update fx.h

--- fx.h
+++ fx.h
@@ -1,16 +1,16 @@
-// fx.h
 #ifndef FX_H
 #define FX_H
-
 #include "engine.h"
 #include "helpers.h"
 #include "game.h"
-
+#include "sprites.h"
+namespace fx{
 ParticleEmitter bones(); // 骨头特效函数
 ParticleEmitter trail(); // 轨迹特效函数
-ParticleEmitter cloud(Rectangle area, std::vector<std::vector<std::string>> variants); // 云特效函数
+ParticleEmitter cloud(Rectangle area, std::vector<std::vector<Sprite>> variants); // 云特效函数
 ParticleEmitter royalty(); // 皇室特效函数
 ParticleEmitter dust(); // 灰尘特效函数
 ParticleEmitter resurrect(GameObject& unit); // 复活特效函数
+}
 
-#endif
+#endif // FX_H
