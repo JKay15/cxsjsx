7d57e7203def649e7efad086e46beaa511cc0f1a
JKay Xiong <xiongjk15@gmail.com>
2023-07-05 00:52:19 +0800

    Update actions.cpp

--- actions.cpp
+++ actions.cpp
@@ -1,47 +1,45 @@
 // actions.cpp
 #include "actions.h"
-#include "sprites.json.h"
-#include "fx.h"
 
 // 造成伤害函数实现
-void Damage(GameObject& object, int amount, GameObject* dealer) {
+void Damage(GameObject* object, int amount, GameObject* dealer) {
     // 创建伤害对象
-    Damage damage = { amount, dealer };
+    struct Damage damage = { amount, dealer };
     // 调用对象的 onDamage 函数
-    object.onDamage(damage);
+    object->onDamage(damage);
     // 更新对象的生命值
-    object.hp = clamp(object.hp - damage.amount, 0, object.maxHp);
+    object->hp = clamp(object->hp - damage.amount, 0, object->maxHp);
     // 如果对象生命值为 0，则调用死亡函数
-    if (!object.hp) Die(object, dealer);
+    if (!object->hp) actions::Die(object, dealer);
 }
 
 // 死亡函数实现
-void Die(GameObject& object, GameObject* killer) {
+void Die(GameObject* object, GameObject* killer) {
     // 创建死亡对象
     Death death = {
         object,
         killer,
-        object.souls
+        object->souls
     };
 
     // 如果对象具有 MOBILE 标签
-    if (object.is(MOBILE)) {
+    if (object->is(MOBILE)) {
         // 获取对象中心坐标
-        auto center = object.center();
+        auto center = object->center();
 
         // 播放骨头特效
         fx::bones()
-            .extend(center)
+            .extend(center.x,center.y)
             .burst(2 + randomInt(3))
             .remove();
 
         // 遍历游戏中的仪式列表，调用仪式的 onDeath 函数（如果存在）
-        for (auto& ritual : game.rituals) {
-            if (ritual.onDeath) ritual.onDeath(death);
+        for (auto* ritual : game.rituals) {
+            if (ritual->onDeath) ritual->onDeath(death);
         }
 
         // 如果随机数小于等于对象的尸体概率，则生成尸体
-        if (randomFloat() <= object.corpseChance) {
+        if (randomFloat() <= object->corpseChance) {
             game.spawn(Corpse(), center.x, center.y);
         }
 
@@ -50,13 +48,13 @@ void Die(GameObject& object, GameObject* killer) {
     }
 
     // 调用对象的 onDeath 函数
-    object.onDeath(death);
+    object->onDeath(death);
 
     // 将对象从游戏中移除
     game.despawn(object);
 }
 
-int castAnimationTimeout = 0;
+std::chrono::milliseconds castAnimationTimeout{0};
 int castGroupId = 1;
 
 // 施法函数实现
@@ -69,9 +67,10 @@ void Cast() {
     spell.casts--;
 
     // 更新玩家精灵为举起双臂状态
-    player.sprite = sprites::norman_arms_up;
-    clearTimeout(castAnimationTimeout);
-    castAnimationTimeout = setTimeout([](){ game.player.sprite = sprites::norman_arms_down; }, 500);
+    player->sprite = sprites::norman_arms_up;
+    castAnimationTimeout = std::chrono::milliseconds(500);
+    std::this_thread::sleep_for(castAnimationTimeout);
+    player->sprite = sprites::norman_arms_down;
 
     int power = spell.basePower;
     float targetAngle = spell.targetAngle - (spell.shotsPerRound * spell.shotOffsetAngle / 2);
@@ -81,7 +80,9 @@ void Cast() {
     for (int j = 0; j < spell.shotsPerRound; j++) {
         auto projectile = Spell();
         float angle = targetAngle + j * spell.shotOffsetAngle;
-        auto [vx, vy] = vectorFromAngle(angle);
+        auto res = vectorFromAngle(angle);
+        double vx = res[0];
+        double vy = res[1];
         auto [x, y] = game.getCastingPoint();
         projectile.x = x - projectile.sprite[2] / 2;
         projectile.y = y - projectile.sprite[3] / 2;
@@ -89,7 +90,7 @@ void Cast() {
         projectile.vy = vy * power;
         projectile.groupId = groupId;
         game.spawn(projectile);
-        game.onCast(projectile);
+        game.onCast(&projectile);
     }
 }
 
@@ -105,16 +106,18 @@ void Resurrect() {
 
     // 遍历游戏中的仪式列表，调用仪式的 onResurrect 函数（如果存在）
     for (auto& ritual : game.rituals) {
-        if (ritual.onResurrect) ritual.onResurrect();
+        if (ritual->onResurrect) ritual->onResurrect();
     }
 
     // 获取游戏中所有尸体对象
-    auto corpses = game.objects.filter([](GameObject& object){ return object.is(CORPSE); });
+    std::vector<GameObject> corpses;
+    std::copy_if(game.objects.begin(), game.objects.end(), std::back_inserter(corpses),
+        [](const GameObject& object) { return object.is(CORPSE); });
 
     // 遍历尸体对象列表
     for (auto& corpse : corpses) {
         // 将尸体从游戏中移除
-        game.despawn(corpse);
+        game.despawn(&corpse);
 
         // 创建骷髅单位并将其加入游戏
         auto unit = Skeleton();
@@ -123,7 +126,7 @@ void Resurrect() {
 
         // 遍历游戏中的仪式列表，调用仪式的 onResurrection 函数（如果存在）
         for (auto& ritual : game.rituals) {
-            if (ritual.onResurrection) ritual.onResurrection(unit);
+            if (ritual->onResurrection) ritual->onResurrection(&unit);
         }
     }
 }
