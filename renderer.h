96367d12454d4a00c0b4d4126b0add618ca2b36c
acetocarmineboss <133663873+acetocarmineboss@users.noreply.github.com>
2023-07-06 22:06:54 +0800

    Create renderer.h

---
+++ renderer.h
@@ -0,0 +1,22 @@
+// renderer.h
+#include <QPainter>
+#include<sprites.h>
+#include <engine.h>
+#include <helpers.h>
+#include <game.h>
+#include <shop.h>
+#include <behaviours.h>
+
+class Renderer {
+public:
+    void screenshake(QPainter &painter, double time);
+    Point screenToSceneCoords(QPainter &painter, double x, double y);
+    void render(QPainter &painter, double dt);
+    void drawShop(QPainter &painter);
+    void drawHud(QPainter &painter);
+    void drawOrbs(double x, double y, double value, double maxValue, const Sprite &sprite, const Sprite &emptySprite);
+    void drawObjects(QPainter &painter);
+    void drawBackground();
+    void drawReticle();
+    void drawParticles();
+};
